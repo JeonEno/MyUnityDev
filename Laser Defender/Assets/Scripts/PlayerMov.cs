03bf56457f4681955a5f011697c386228e40b216
Eno Jeon <gatojih0817@gmail.com>
2023-10-04 20:38:32 +0900

    Add PlayerMov Scripts in LazerDefender

---
+++ Laser Defender/Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    vector2 rawInput;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
