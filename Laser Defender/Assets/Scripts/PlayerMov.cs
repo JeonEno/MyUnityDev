195fe69ee259906cb84769e916516a66e2b8d80f
Eno Jeon <gatojih0817@gmail.com>
2023-10-05 19:02:14 +0900

    add inputsystem to project and player prefab

--- Laser Defender/Assets/Scripts/PlayerMov.cs
+++ Laser Defender/Assets/Scripts/PlayerMov.cs
@@ -3,12 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMov : MonoBehaviour
-{
-    void Start()
-    {
-        
-    }
-
+{  
     void Update()
     {
         
